e8b589e4900f9562cdaef55e8de9591ab35475d0
Henrik Joreteg <hjoreteg@gmail.com>
2015-05-07 14:19:55 -0700

    Thu May  7 14:19:55 PDT 2015

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 var app = require('ampersand-app')
 var Router = require('ampersand-router')
 var qs = require('qs')
+var xhr = require('xhr')
 var ReposPage = require('./pages/repos')
 var PublicPage = require('./pages/public')
 
@@ -36,8 +37,38 @@ module.exports = Router.extend({
   authCallback: function () {
     var query = qs.parse(window.location.search.slice(1))
     console.log(query.code)
+
+    xhr({
+      url: 'https://labelr-localhost.herokuapp.com/authenticate/' + query.code,
+      json: true
+    }, function (err, response, body) {
+      console.log(body)
+    })
   }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
