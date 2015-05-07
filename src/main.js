5ab35657d8036673f56856014a1b8bfb4f367335
Henrik Joreteg <hjoreteg@gmail.com>
2015-05-07 11:54:04 -0700

    Thu May  7 11:54:04 PDT 2015

--- src/main.js
+++ src/main.js
@@ -1,7 +1,37 @@
+var app = require('ampersand-app')
 var View = require('ampersand-view')
+var ViewSwitcher = require('ampersand-view-switcher')
 var template = require('./templates/body.jade')
 
 module.exports = View.extend({
   template: template,
-  autoRender: true
+
+  autoRender: true,
+
+  initialize: function () {
+    this.listenTo(app, 'page', this.onPage)
+  },
+
+  render: function () {
+    this.renderWithTemplate()
+    this.pageSwitcher = new ViewSwitcher(this.queryByHook('page-container'))
+  },
+
+  onPage: function (page) {
+    this.pageSwitcher.set(page)
+  }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
