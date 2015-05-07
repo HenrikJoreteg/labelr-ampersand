329a291cb6fdc76198b2847d877cdb7959d7551c
Henrik Joreteg <hjoreteg@gmail.com>
2015-05-07 14:08:42 -0700

    Thu May  7 14:08:42 PDT 2015

--- src/router.js
+++ src/router.js
@@ -1,12 +1,16 @@
 var app = require('ampersand-app')
 var Router = require('ampersand-router')
+var qs = require('qs')
 var ReposPage = require('./pages/repos')
 var PublicPage = require('./pages/public')
 
 module.exports = Router.extend({
   routes: {
     '': 'public',
-    'repos': 'repos'
+    'repos': 'repos',
+    'login': 'login',
+    'logout': 'logout',
+    'auth/callback': 'authCallback'
   },
 
   public: function () {
@@ -15,5 +19,57 @@ module.exports = Router.extend({
 
   repos: function () {
     app.trigger('page', new ReposPage())
+  },
+
+  login: function () {
+    window.location = 'https://github.com/login/oauth/authorize?' + qs.stringify({
+      client_id: 'f8dd69187841cdd22a26',
+      scope: 'user,repo',
+      redirect_uri: window.location.origin + '/auth/callback'
+    })
+  },
+
+  logout: function () {
+
+  },
+
+  authCallback: function () {
+    var query = qs.parse(window.location.search.slice(1))
+    console.log(query.code)
   }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
