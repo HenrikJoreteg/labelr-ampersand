3f21572d3edaacbf3b35bc35c0c28684608bf8c1
Henrik Joreteg <hjoreteg@gmail.com>
2015-05-08 13:49:21 -0700

    Fri May  8 13:49:21 PDT 2015

--- src/models/label.js
+++ src/models/label.js
@@ -1,8 +1,67 @@
+var app = require('ampersand-app')
 var Model = require('ampersand-model')
+var xhr = require('xhr')
+var githubMixin = require('../helpers/github-mixin')
 
-module.exports = Model.extend({
+module.exports = Model.extend(githubMixin, {
+  idAttribute: 'name',
+
   props: {
-    'name': 'string',
-    'color': 'string'
+    name: 'string',
+    color: 'string'
+  },
+
+  session: {
+    editing: {
+      type: 'boolean',
+      default: false
+    }
+  },
+
+  derived: {
+    style: {
+      deps: ['color'],
+      fn: function () {
+        return 'background-color: #' + this.color
+      }
+    }
+  },
+
+  update: function (newAttributes) {
+    var oldAttributes = this.attributes
+    var self = this
+
+    xhr({
+      url: this.url(),
+      json: newAttributes,
+      method: 'PATCH',
+      headers: {
+        'Authorization': 'token ' + app.me.token
+      }
+    }, function (err, resp, body) {
+      if (err) {
+        console.error('something went wrong')
+        self.set(oldAttributes)
+      }
+    })
+    this.set(newAttributes)
   }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/views/label-item.js
+++ src/views/label-item.js
@@ -2,5 +2,79 @@ var View = require('ampersand-view')
 var template = require('../templates/views/label-item.jade')
 
 module.exports = View.extend({
-  template: template
+  template: template,
+
+  bindings: {
+    'model.editing': {
+      type: 'toggle',
+      yes: '[data-hook=editing]',
+      no: '[data-hook=viewing]'
+    },
+
+    'model.style': {
+      type: 'attribute',
+      name: 'style',
+      hook: 'color-dot'
+    },
+
+    'model.name': {
+      hook: 'name'
+    }
+  },
+
+  events: {
+    'click [data-hook=edit]': 'onEditClick',
+    'click [data-hook=cancel]': 'onCancelClick',
+    'click [data-hook=delete]': 'onDeleteClick',
+    'submit form': 'onFormSubmit'
+  },
+
+  render: function () {
+    this.renderWithTemplate()
+    this.cacheElements({
+      nameInput: 'input[name=name]',
+      colorInput: 'input[name=color]'
+    })
+  },
+
+  onEditClick: function () {
+    this.model.editing = true
+    this.nameInput.value = this.model.name
+    this.colorInput.value = this.model.color
+  },
+
+  onCancelClick: function () {
+    this.model.editing = false
+  },
+
+  onDeleteClick: function () {
+    this.model.destroy()
+  },
+
+  onFormSubmit: function (event) {
+    event.preventDefault()
+    this.model.update({
+      name: this.nameInput.value.trim(),
+      color: this.colorInput.value.trim()
+    })
+    this.model.editing = false
+  }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
